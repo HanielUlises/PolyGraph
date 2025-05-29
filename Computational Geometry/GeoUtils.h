eb33e2d526005df05546ce04f90ffc39435e06f8
Haniel Ulises <88525113+HanielUlises@users.noreply.github.com>
2025-05-29 10:07:58 -0600

    Fixed header

--- Computational Geometry/GeoUtils.h
+++ Computational Geometry/GeoUtils.h
@@ -3,7 +3,7 @@
 #include "Point.h"
 
 namespace jmk{
-    double area_triangle_2d();
+    double area_triangle_2d(const Point2d &a, const Point2d &b, const Point2d &c);
 
     int orientation_2d();
 }
