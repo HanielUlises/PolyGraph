8fa6efe70ee6bcf221cfaa0a5301f24df24c769f
HanielUlises <hanielulises2003@gmail.com>
2025-07-03 23:02:47 -0600

    Line representation

--- Computational Geometry/Line.h
+++ Computational Geometry/Line.h
@@ -5,5 +5,25 @@ namespace GeomCore{
     class Line{
         Vector<coord_type, dim> point;
         Vector<coord_type, dim> dir;
-    };    
+        
+        public:
+            Line(){}
+            Line(Vector<coord_type, dim> &p1, Vector<coord_type, dim> &p2){
+                dir = p2 - p1;
+                point = p1;
+            }
+
+            Vector<coord_type, dim> get_point();
+            Vector<coord_type, dim> get_direction();
+    };
+    
+    template <class coord_type, size_t dim>
+    inline Vector<coord_type, dim> Line<coord_type, dim>::get_point(){
+        return point;
+    }
+
+    template <class coord_type, size_t dim>
+    inline Vector<coord_type, dim> Line<coord_type, dim>::get_direction(){
+        return point;
+    }
 }
