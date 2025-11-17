de53a3bf80327ac2c16d3332506cc52d8f02249d
HanielUlises <hanielulises2003@gmail.com>
2025-11-16 21:54:16 -0600

    Representation of plane

---
+++ Computational Geometry/Plane.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Vector.h"
+#include "Point.h"
+
+namespace GeomCore{
+    template <class coord_type>
+    class Plane{
+            Vector3f normal;
+            float d = 0;
+        public:
+            Plane(){}
+
+            Plane(Vector3f &_normal, float _constant) : normal(_normal), d(_constant) {}
+
+            Plane(Point3d &_p1, Point3d &_p2, Point3d &_p3){
+                auto v12 = _p2 - _p1;
+                auto v13 = _p3 - _p1;
+
+                normal = cross_product_R3(v12, v13);
+                d = dot_product(normal, _p1);
+            }
+    };
+}
