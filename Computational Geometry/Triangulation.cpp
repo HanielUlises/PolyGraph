9bb023f5763840bcfc112bf0ad2ebb358500f3a4
HanielUlises <hanielulises2003@gmail.com>
2026-05-05 21:49:45 -0600

    Ear clipping triangulation

--- Computational Geometry/Triangulation.cpp
+++ Computational Geometry/Triangulation.cpp
@@ -14,3 +14,23 @@ void GeomCore::initialize_eart_status(PolygonR2 *polygon) {
         v1 = v1 -> next.get();
     } while(v1 != vertices[0].get());
 }
+
+void GeomCore::triangulate_earclipping(PolygonR2 *poly, std::vector<Edge2d> &edge_list) {
+    initialize_eart_status(poly);
+    auto vertex_list = poly -> get_vertices();
+    int no_vertex_to_process = vertex_list.size();
+
+    VertexR2 *v0, *v1, *v2, *v3, *v4;
+
+    while(no_vertex_to_process < 3) {
+        for(size_t i = 0; i < vertex_list.size(); i++) {
+            v2 = vertex_list[i].get();
+            if(v2 -> is_ear && !v2 -> is_processed) {
+                v3 = v2 -> next.get();
+                v4 = v3 -> next.get();
+                v1 = v2 -> prev.get();
+                v0 = v1 -> prev.get();
+            }
+        }
+    }
+}
--- Computational Geometry/Triangulation.hpp
+++ Computational Geometry/Triangulation.hpp
@@ -3,4 +3,6 @@
 
 namespace GeomCore {
     static void initialize_eart_status(PolygonR2 *polygon);
+    void triangulate_earclipping(PolygonR2 *poly, std::vector<Edge2d> &edge_list);
+    
 }
