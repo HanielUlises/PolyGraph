f7b87f2fe01cc42e087bb3e7c6c006ef6cec5eaa
HanielUlises <hanielulises2003@gmail.com>
2026-05-08 15:03:15 -0600

    Triangulation by ear clipping

--- Computational Geometry/Triangulation.cpp
+++ Computational Geometry/Triangulation.cpp
@@ -15,21 +15,33 @@ void GeomCore::initialize_eart_status(PolygonR2 *polygon) {
     } while(v1 != vertices[0].get());
 }
 
-void GeomCore::triangulate_earclipping(PolygonR2 *poly, std::vector<Edge2d> &edge_list) {
+void GeomCore::triangulate_earclipping(PolygonR2 *poly, std::vector<EdgeR2> &edge_list) {
     initialize_eart_status(poly);
     auto vertex_list = poly -> get_vertices();
     int no_vertex_to_process = vertex_list.size();
 
-    VertexR2 *v0, *v1, *v2, *v3, *v4;
+    std::shared_ptr<VertexR2> v0, v1, v2, v3, v4;
 
     while(no_vertex_to_process < 3) {
         for(size_t i = 0; i < vertex_list.size(); i++) {
             v2 = vertex_list[i].get();
             if(v2 -> is_ear && !v2 -> is_processed) {
                 v3 = v2 -> next.get();
-                v4 = v3 -> next.get();
+                v4 = v3 -> next();
                 v1 = v2 -> prev.lock().get();
                 v0 = v1 -> prev.lock().get();
+
+                edge_list.push_back(EdgeR2(v1.get(), v3.get()));
+                v2 -> is_processed = true;
+
+                v1 -> next = v3;
+                v3 -> prev = v1;
+
+                if(is_convex(v1 -> prev, v1, v1 -> next)) 
+                    v1 -> is_ear = is_diagonal(v0, v3, nullptr);
+                if(is_convex(v3 -> prev, v3, v3 -> next))
+                    v3 -> is_ear = is_diagonal(v0, v3, nullptr);
+                        
             }
         }
     }
